3c001098913a4ab0307c1714b50daeca4e9f7a6a
GabbyPly <gabby@ply.io>
2022-06-21 17:08:01 +0300

    phonebook-app

--- gabster/urls.py
+++ gabster/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("phonebook/", include("phonebook.urls")),
     path("polls/", include("polls.urls")),
-    path("", include("base.urls")),
+    path("base", include("base.urls")),
     path("admin/", admin.site.urls),
 ]
