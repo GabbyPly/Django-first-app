efddd03f08dfe5c77ed7d985b170d7b8653d80e6
GabbyPly <gabby@ply.io>
2022-06-22 15:24:56 +0300

    c

--- phonebook/models.py
+++ phonebook/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # https://github.com/stefanfoulis/django-phonenumber-field, https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-a-phone-number-in-django-models
-class PhonebookContact(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+class Contact(models.Model):
+    created = models.DateTimeField(auto_now_add=True, blank=True)
     name = models.CharField(max_length=255)
-    number = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    number = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    # code = models.TextField()
 
     class Meta:
         ordering = ["created"]
