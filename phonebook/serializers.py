3f70d359fe84692d16f952f9fe053347fa8f5541
GabbyPly <gabby@ply.io>
2022-06-24 18:27:06 +0300

    Serializers + url-naming

--- phonebook/serializers.py
+++ phonebook/serializers.py
@@ -4,18 +4,18 @@ from phonebook.models import Contact
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    contacts = serializers.HyperlinkedRelatedField(many=True, view_name="snippet-detail", read_only=True)
-
-    class Meta:
-        model = User
-        fields = ["url", "id", "username", "contacts"]
-
-
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     highlight = serializers.HyperlinkedIdentityField(view_name="contact-highlight", format="html")
 
     class Meta:
         model = Contact
-        fields = ["url", "id", "name", "linenos", "language", "style", "code", "owner", "highlighted"]
+        fields = ["url", "id", "name", "linenos", "language", "style", "code", "owner", "highlight"]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    contacts = serializers.HyperlinkedRelatedField(many=True, view_name="contact-detail", read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["url", "id", "username", "contacts"]
--- phonebook/urls.py
+++ phonebook/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import views
+
+from phonebook import views
 
 app_name = "phonebook"
 urlpatterns = [
     path("", views.api_root),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view()),
-    path("phonebook/<int:pk>/highlight/", views.ContactHighlight.as_view()),
-    path("phonebook/", views.ContactList.as_view()),
-    path("phonebook/<int:pk>/", views.ConatctDetail.as_view()),
+    path("phonebook/", views.ContactList.as_view(), name="contact-list"),
+    path("phonebook/<int:pk>/", views.ConatctDetail.as_view(), name="contact-detail"),
+    path("phonebook/<int:pk>/highlight/", views.ContactHighlight.as_view(), name="contact-highlight"),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
 ]
 
 
