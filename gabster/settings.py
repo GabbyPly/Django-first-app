56a1a0c8b5516b3acd3c2276a9ed7ffe05f3c403
GabbyPly <gabby@ply.io>
2022-06-25 13:43:42 +0300

    Add pagination

--- gabster/settings.py
+++ gabster/settings.py
@@ -132,3 +132,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
+
+REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination", "PAGE_SIZE": 3}
