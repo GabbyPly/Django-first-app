474f3c88bcb4c5b53905b29847a2c3606218a26e
GabbyPly <gabby@ply.io>
2022-06-26 14:22:49 +0300

    add router

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -1,28 +1,13 @@
-from collections import UserList
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from phonebook import views
-from phonebook.views import ContactViewSet, UserViewSet, api_root
-from rest_framework import renderers
 
-contact_list = ContactViewSet.as_view({"get": "list", "post": "create"})
 
-contact_detail = ContactViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
-)
+router = DefaultRouter()
+router.register(r"contacts", views.ContactViewSet, basename="contacts")
+router.register(r"users", views.UserViewSet, basename="users")
 
-contact_highlight = ContactViewSet.as_view({"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
-
-# API endpoints
-urlpatterns = format_suffix_patterns(
-    [
-        path("", views.api_root),
-        path("contacts/", contact_list, name="contact-list"),
-        path("contacts/<int:pk>/", contact_detail, name="contact-detail"),
-        path("contacts/<int:pk>/highlight/", contact_highlight, name="contact-highlight"),
-        path("users/", user_list, name="user-list"),
-        path("users/<int:pk>/", user_detail, name="user-detail"),
-    ]
-)
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path("", include(router.urls)),
+]
