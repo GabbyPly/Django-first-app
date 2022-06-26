a1e308ed43d1cf70a2edf4b77f86b5c70f4f39dc
GabbyPly <gabby@ply.io>
2022-06-26 17:40:21 +0300

    add basename. These basenames will generate viewnames such as contact-list, contact-detail, etc...

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -1,19 +1,12 @@
+from posixpath import basename
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from phonebook import views
 
-# basename is breaking
-# If you want to include the basename use user & contact.
-# The reason is probably: that they need to match the start of the internal view's name
+
 router = DefaultRouter()
-router.register(
-    r"contacts",
-    views.ContactViewSet,
-)
-router.register(
-    r"users",
-    views.UserViewSet,
-)
+router.register(r"contacts", views.ContactViewSet, basename="contact")
+router.register(r"users", views.UserViewSet, basename="user")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
