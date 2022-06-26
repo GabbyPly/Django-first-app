66361a9d3b64285a07fd3f78c11f64a2ef44cc25
GabbyPly <gabby@ply.io>
2022-06-26 14:23:11 +0300

    delete api_root method, router takes care of that for us

--- phonebook/views.py
+++ phonebook/views.py
@@ -12,16 +12,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 
 
-@api_view(["GET"])
-def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "contacts": reverse("contact-list", request=request, format=format),
-        }
-    )
-
-
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `retrieve` actions.
