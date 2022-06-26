9d226212dd429c5fa3faa413ecae549ed67cc8d3
GabbyPly <gabby@ply.io>
2022-06-26 14:32:12 +0300

    basename is breaking

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -2,10 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from phonebook import views
 
-
+# basename is breaking
 router = DefaultRouter()
-router.register(r"contacts", views.ContactViewSet, basename="contacts")
-router.register(r"users", views.UserViewSet, basename="users")
+router.register(
+    r"contacts",
+    views.ContactViewSet,
+)
+router.register(
+    r"users",
+    views.UserViewSet,
+)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
