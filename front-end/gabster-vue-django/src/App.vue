e2b9a4b083cf3b388bfb27b485fab9786a9599bf
GabbyPly <gabby@ply.io>
2022-07-26 17:57:33 +0300

    Add VueRouter

--- front-end/gabster-vue-django/src/App.vue
+++ front-end/gabster-vue-django/src/App.vue
@@ -1,43 +1,10 @@
 <template>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <div id="root">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <h1>Hello World</h1>
-    <Button @isr-uni="loadUnis" />
-    <UniversitiesList @delete-uny="deleteUny" :universities="unis" />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import Button from "./components/Button.vue";
-import UniversitiesList from "./components/UniversitiesList.vue";
-
-export default {
-  name: "App",
-  methods: {
-    loadUnis(allUnis) {
-      this.$data.unis = allUnis;
-    },
-    deleteUny(unyToDelete) {
-      confirm(`Are you sure you want to delete ${unyToDelete}?`);
-      this.$data.unis = this.$data.unis.filter(
-        (uny) => uny.name !== unyToDelete
-      );
-    },
-  },
-  components: {
-    // HelloWorld,
-    Button,
-    UniversitiesList,
-  },
-  data() {
-    return {
-      unis: [],
-    };
-  },
-};
-</script>
 
 <style>
 #app {
@@ -46,13 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
-/* /* import the fontawesome core */ import { library } from
-'@fortawesome/fontawesome-svg-core' /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' /* import
-specific icons */ import { faUserSecret } from
-'@fortawesome/free-solid-svg-icons' /* add icons to the library */
-library.add(faUserSecret) createApp(App) .component('font-awesome-icon',
-FontAwesomeIcon) .mount('#app') */
