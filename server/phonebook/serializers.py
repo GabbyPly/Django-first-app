0e77a26a718128f06824a119dbff10f1c236f8cc
GabbyPly <gabby@ply.io>
2022-08-04 14:53:47 +0300

    change UserSerializer for sign up

--- server/phonebook/serializers.py
+++ server/phonebook/serializers.py
@@ -13,9 +13,22 @@ class ContactSerializer(serializers.HyperlinkedModelSerializer):
         fields = ["url", "id", "name", "linenos", "language", "style", "code", "owner", "highlight"]
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     contacts = serializers.HyperlinkedRelatedField(many=True, view_name="contact-detail", read_only=True)
 
     class Meta:
         model = User
-        fields = ["url", "id", "username", "contacts"]
+        fields = ["url", "id", "username", "contacts", "password"]
+        extra_kwargs = {"password": {"write_only": True}}  # note pw/password
+
+    def create(self, validated_data):
+        print("val data", validated_data)
+        pw = validated_data.pop("password", None)
+        # user_instance = User(**validated_data)
+        user_instance = self.Meta.model(**validated_data)
+        if pw is not None:
+            user_instance.set_password(pw)
+        user_instance.save()
+        print("user_instance")
+        print(user_instance)
+        return user_instance
