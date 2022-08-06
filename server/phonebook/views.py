127d4f84ae61bbf9b8a5d2c4a2db76f11754f9cc
GabbyPly <gabby@ply.io>
2022-08-06 23:23:29 +0300

    Login protect /contacts and redirect back to it.

--- server/phonebook/views.py
+++ server/phonebook/views.py
@@ -1,4 +1,6 @@
 from multiprocessing import AuthenticationError
+
+from django.shortcuts import redirect
 from phonebook.models import Contact
 from phonebook.serializers import ContactSerializer, UserSerializer
 from rest_framework import generics
@@ -14,6 +16,8 @@ from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
 
 
 class RegisterViewSet(viewsets.ModelViewSet):
@@ -31,6 +35,7 @@ class LoginView(APIView):
         print("request", request)
         username = request.data["username"]
         password = request.data["password"]
+        return_to = request.GET.get("return_to", None)
 
         # user = User.objects.get(email=email)
         user = User.objects.filter(username=username).first()
@@ -42,11 +47,20 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationError("Incorrect password")
 
-        # return JsonResponse(user)
+        #  Should have used the 'authenticate' method from 'django.contrib.auth' ?
+        login(request, user)
+        data = request.data
+        if return_to is not None:
+            print("return to is", return_to)
+            response = redirect(return_to)  # redirecting to home page
+            return response
+
         return Response({"success": True})
 
 
-class ContactViewSet(viewsets.ModelViewSet):
+class ContactViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
+    login_url = "/login/"
+    redirect_field_name = "return_to"
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
