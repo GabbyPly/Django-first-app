70b34af29941f08aed693c937d4121274eacdd93
GabbyPly <gabby@ply.io>
2022-08-06 22:38:14 +0300

    Switch users to register view

--- server/phonebook/urls.py
+++ server/phonebook/urls.py
@@ -6,9 +6,11 @@ from phonebook import views
 
 router = DefaultRouter()
 router.register(r"contacts", views.ContactViewSet, basename="contact")
-router.register(r"users", views.UserViewSet, basename="user")
+router.register(r"register", views.RegisterViewSet, basename="user")
+
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path("", include(router.urls)),
+    path("login/", views.LoginView.as_view()),
 ]
--- server/phonebook/views.py
+++ server/phonebook/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import AuthenticationError
 from phonebook.models import Contact
 from phonebook.serializers import ContactSerializer, UserSerializer
 from rest_framework import generics
@@ -10,15 +11,39 @@ from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+from django.http import JsonResponse
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class RegisterViewSet(viewsets.ModelViewSet):
     """
-    This viewset automatically provides `list` and `retrieve` actions.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
     """
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        print("request", request)
+        username = request.data["username"]
+        password = request.data["password"]
+
+        # user = User.objects.get(email=email)
+        user = User.objects.filter(username=username).first()
+        print("user", user)
+
+        if user is None:
+            raise AuthenticationFailed("User not found")
+
+        if not user.check_password(password):
+            raise AuthenticationError("Incorrect password")
+
+        # return JsonResponse(user)
+        return Response({"success": True})
 
 
 class ContactViewSet(viewsets.ModelViewSet):
