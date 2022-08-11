0052daf649edbe7ad6d89da33d7889aacef61af8
GabbyPly <gabby@ply.io>
2022-08-11 22:39:24 +0300

    Change keyword to bearer

--- server/gabster/settings.py
+++ server/gabster/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",  # <-- And here
+        "gabster.authentication.BearerAuthentication",  # <-- And here
     ],
     # You can use this if you want to limit all of the API for authenticatedUsers
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
