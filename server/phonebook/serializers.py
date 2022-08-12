c885e1eb2e5270af9f3ab1c9abfe6b9e075ade44
GabbyPly <gabby@ply.io>
2022-08-12 10:04:01 +0300

    Deleted userSerializer by mistake

--- server/phonebook/serializers.py
+++ server/phonebook/serializers.py
@@ -19,21 +19,22 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ["id", "created", "title", "content", "author"]
 
     # owner = serializers.ReadOnlyField(source="owner.username")class UserSerializer(serializers.ModelSerializer):
-    contacts = serializers.HyperlinkedRelatedField(many=True, view_name="contact-detail", read_only=True)
+    class UserSerializer(serializers.ModelSerializer):
+        contacts = serializers.HyperlinkedRelatedField(many=True, view_name="contact-detail", read_only=True)
 
-    class Meta:
-        model = User
-        fields = ["url", "id", "username", "contacts", "password"]
-        extra_kwargs = {"password": {"write_only": True}}  # note pw/password
+        class Meta:
+            model = User
+            fields = ["url", "id", "username", "contacts", "password"]
+            extra_kwargs = {"password": {"write_only": True}}  # note pw/password
 
-    def create(self, validated_data):
-        print("val data", validated_data)
-        pw = validated_data.pop("password", None)
-        # user_instance = User(**validated_data)
-        user_instance = self.Meta.model(**validated_data)
-        if pw is not None:
-            user_instance.set_password(pw)
-        user_instance.save()
-        print("user_instance")
-        print(user_instance)
-        return user_instance
+        def create(self, validated_data):
+            print("val data", validated_data)
+            pw = validated_data.pop("password", None)
+            # user_instance = User(**validated_data)
+            user_instance = self.Meta.model(**validated_data)
+            if pw is not None:
+                user_instance.set_password(pw)
+            user_instance.save()
+            print("user_instance")
+            print(user_instance)
+            return user_instance
