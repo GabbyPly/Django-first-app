0e9c076a754e3f0b87370d9eb935707e96b00f5e
GabbyPly <gabby@ply.io>
2022-11-29 18:47:57 +0200

    Matching brackets, auto open/close brackets, styling etc...

--- front-end/gabster-vue-django/src/views/CodeMirrorView.vue
+++ front-end/gabster-vue-django/src/views/CodeMirrorView.vue
@@ -1,31 +1,75 @@
 <template>
-  <textarea class="what" name="" id="some" cols="30" rows="10"></textarea>
+  <textarea
+    v-model="code"
+    class="what"
+    name=""
+    ref="editor"
+    id="editor"
+    cols="30"
+    rows="10"
+  ></textarea>
 </template>
 
 <script>
 import "codemirror/addon/edit/matchbrackets";
+import "codemirror/addon/edit/closebrackets";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript";
 import CM from "codemirror";
-console.log("CM", CM);
-console.log("CM.fromTextArea", CM.fromTextArea);
-const myCodeMirror = CM.fromTextArea(document.getElementById("some"), {
-  value: "function myScript(){return 100;}\n",
-  mode: "javascript", // { name: "javascript", json: true }
-  theme: "darcula",
-  lineNumbers: true,
-  // lineWrapping: true,
-});
-console.log("hey created  myCodeMirror", myCodeMirror);
+/*
+// created() {
+  //   const myCodeMirror = CM.fromTextArea(document.getElementById("editor"), {
+  //     value: "function myScript(){return 100;}\n",
+  //     mode: "javascript", // { name: "javascript", json: true }
+  //     theme: "darcula",
+  //     lineNumbers: true,
+  //     lineWrapping: true,
+  //   });
+  //   console.log("hey created  myCodeMirror", myCodeMirror);
+  // },
+*/
 
 import { defineComponent } from "vue";
 
 import "codemirror/theme/darcula.css";
 
-export default {
+export default defineComponent({
   components: {}, // Codemirror,
+  created() {
+    const editor = document.getElementById("editor");
+    console.log("created  editor", editor);
+  },
+  computed: {
+    code() {
+      return "function myScript(){return 100;}\n";
+    },
+  },
+  mounted() {
+    console.log("mounted");
+    console.log("refs", this.$refs);
+    const editor = this.$refs.editor;
+    if (editor) {
+      // document
+      //   .getElementsByClassName("CodeMirror-scroll")
+      //   .style.setProperty("text-align", "left"); // left !important
+      console.log("mounted  scroll", scroll);
+      const myCodeMirror = CM.fromTextArea(editor, {
+        value: "function myScript(){return 100;}\n",
+        mode: "javascript", // { name: "javascript", json: true }
+        theme: "darcula",
+        lineNumbers: true,
+        spellcheck: true,
+        matchingbrackets: true,
+        autoCloseBrackets: true,
+        // lineWrapping: true,
+      });
+      myCodeMirror.setOption("closebrackets", true);
+      console.log({ CM });
+      console.log("hey created  myCodeMirror", myCodeMirror);
+    }
+  },
   // created() {
-  //   const myCodeMirror = CM.fromTextArea(document.getElementById("some"), {
+  //   const myCodeMirror = CM.fromTextArea(document.getElementById("editor"), {
   //     value: "function myScript(){return 100;}\n",
   //     mode: "javascript", // { name: "javascript", json: true }
   //     theme: "darcula",
@@ -34,14 +78,10 @@ export default {
   //   });
   //   console.log("hey created  myCodeMirror", myCodeMirror);
   // },
-};
+});
 </script>
 <style lang="css" scoped>
-#some {
+.what {
   text-align: left !important;
-}
-.CodeMirror-scroll {
-  color: brown;
-  /* text-align: left !important; */
 }
 </style>
